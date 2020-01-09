0e5baec1343e6cd5837e53f9f6887828ecb24a16
talwinder.kaur <talwinder.kaur@securekey.com>
2020-01-09 14:12:34 -0500

    feat: Add data CMS using strapi
    closes: #2
    
    Signed-off-by: talwinder.kaur <talwinder.kaur@securekey.com>

--- scripts/check_unit.sh
+++ scripts/check_unit.sh
@@ -33,3 +33,10 @@ go test $PKGS -count=1 -race -coverprofile=profile.out -covermode=atomic -timeou
 amend_coverage_file
 cd "$pwd" || exit
 
+# Running strapi-demo unit tests
+cd cmd/strapi-demo
+PKGS=`go list github.com/trustbloc/edge-store/cmd/strapi-demo/... 2> /dev/null | \
+                                                 grep -v /mocks`
+go test $PKGS -count=1 -race -coverprofile=profile.out -covermode=atomic -timeout=10m
+amend_coverage_file
+cd "$pwd" || exit
