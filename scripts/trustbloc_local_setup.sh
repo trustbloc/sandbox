62c644f90e48e2bb46a5f13156d6d9ba74d0c551
Rolson Quadras <rolson.quadras@securekey.com>
2020-02-05 16:23:31 -0500

    chore: Add DIDComm Router to docker compose
    Signed-off-by: Rolson Quadras <rolson.quadras@securekey.com>

--- scripts/trustbloc_local_setup.sh
+++ scripts/trustbloc_local_setup.sh
@@ -10,9 +10,11 @@ SANDBOX_HOME=~/.trustbloc-local/sandbox/
 mkdir -p ${SANDBOX_HOME}/certs
 cp test/bdd/fixtures/keys/tls/trustbloc-dev-ca.* ${SANDBOX_HOME}/certs
 
+# copy these entries into /etc/hosts file
 cat > ${SANDBOX_HOME}/hosts <<EOF
 127.0.0.1 rp.trustbloc.local
 127.0.0.1 issuer.trustbloc.local
 127.0.0.1 cms.trustbloc.local
 127.0.0.1 myagent.trustbloc.local
+127.0.0.1 router.trustbloc.local
 EOF
