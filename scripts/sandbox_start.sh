54bb90689e840aeff39edcf968184289384a0ecd
Firas Qutishat <firas.qutishat@securekey.com>
2020-02-10 15:32:30 -0500

    chore: setup sidetree-fabric
    closes #91
    
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- scripts/sandbox_start.sh
+++ scripts/sandbox_start.sh
@@ -9,7 +9,9 @@ set -e
 echo "Running $0"
 
 cd test/bdd/fixtures/demo
-dockerComposeFiles='-f docker-compose-third-party.yml -f docker-compose-router.yml -f docker-compose-sidetree-mock.yml -f docker-compose-edge-components.yml -f docker-compose-demo-applications.yml'
+sidetreeCompseFile='-f docker-compose-sidetree-mock.yml'
+if [ "$START_SIDETREE_FABRIC" = true ] ; then
+    sidetreeCompseFile='-f docker-compose-sidetree-fabric.yml'
+fi
+dockerComposeFiles="-f docker-compose-third-party.yml -f docker-compose-router.yml -f docker-compose-edge-components.yml -f docker-compose-demo-applications.yml $sidetreeCompseFile"
 (source .env && docker-compose $dockerComposeFiles down && docker-compose $dockerComposeFiles up --force-recreate)
-
-
