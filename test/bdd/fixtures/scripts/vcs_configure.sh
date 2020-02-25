a263194cc5243dd5b009ec8fc64d3bf1fabe8d5c
Firas Qutishat <firas.qutishat@securekey.com>
2020-02-25 14:05:09 -0500

    fix: fix typo
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- test/bdd/fixtures/scripts/vcs_configure.sh
+++ test/bdd/fixtures/scripts/vcs_configure.sh
@@ -8,4 +8,4 @@
 apk --no-cache add curl
 
 curl -d '{"name":"vc-issuer-1", "uri":"http://vc-issuer-1.com", "signatureType":"Ed25519Signature2018"}' -H "Content-Type: application/json" -X POST http://vcs.example.com:8070/profile
-curl -d '{"name":"vc-issuer-2", "uri":"http://vc-issuer-2.com", "signatureType":"Ed25519Signature2018", "DID":"did:v1:test:nym:z6MkhLbRigh9utJNCaiEAdkqktz4r7yVBFDeaeqCeT7pRFnF","DIDPrivateKey":"5dF8yAW7hjLkJsfMXKqTPdDZUT56dX7Jq7TdXEtUEHHt2YUFAE34nQwyPCEp5XdWCKPSxs69xXqozsNh6MoJTmz5"}' -H "Content-Type: application/json" -X POST http://vcs.example.com:8070/profile
+curl -d '{"name":"vc-issuer-2", "uri":"http://vc-issuer-2.com", "signatureType":"Ed25519Signature2018", "did":"did:v1:test:nym:z6MkhLbRigh9utJNCaiEAdkqktz4r7yVBFDeaeqCeT7pRFnF","didPrivateKey":"5dF8yAW7hjLkJsfMXKqTPdDZUT56dX7Jq7TdXEtUEHHt2YUFAE34nQwyPCEp5XdWCKPSxs69xXqozsNh6MoJTmz5"}' -H "Content-Type: application/json" -X POST http://vcs.example.com:8070/profile
