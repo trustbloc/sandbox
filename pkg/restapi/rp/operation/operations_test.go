a57a12b556eb53901cb2da9bf489a17116599298
Rolson Quadras <rolson.quadras@securekey.com>
2020-04-01 10:58:22 -0400

    refactor: Update to use W3C Verifier APIs
    Signed-off-by: Rolson Quadras <rolson.quadras@securekey.com>

--- pkg/restapi/rp/operation/operations_test.go
+++ pkg/restapi/rp/operation/operations_test.go
@@ -18,41 +18,69 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	edgesvcops "github.com/trustbloc/edge-service/pkg/restapi/vc/operation"
+)
+
+const (
+	validContext = `"@context":["https://www.w3.org/2018/credentials/v1"]`
+
+	validVC = `{` +
+		validContext + `,
+	  "id": "http://example.edu/credentials/1872",
+	  "type": "VerifiableCredential",
+	  "credentialSubject": {
+		"id": "did:example:ebfeb1f712ebc6f1c276e12ec21"
+	  },
+	  "issuer": {
+		"id": "did:example:76e12ec712ebc6f1c221ebfeb1f",
+		"name": "Example University"
+	  },
+	  "issuanceDate": "2010-01-01T19:23:24Z",
+	  "credentialStatus": {
+		"id": "https://example.gov/status/24",
+		"type": "CredentialStatusList2017"
+	  }
+	}`
+
+	validVP = `{
+		"@context": [
+			"https://www.w3.org/2018/credentials/v1",
+			"https://www.w3.org/2018/credentials/examples/v1"
+		],
+		"id": "urn:uuid:3978344f-8596-4c3a-a978-8fcaba3903c5",
+		"type": "VerifiablePresentation",
+		"verifiableCredential": [{
+			"@context": [
+				"https://www.w3.org/2018/credentials/v1",
+				"https://www.w3.org/2018/credentials/examples/v1"
+			],
+			"id": "http://example.edu/credentials/1872",
+			"type": "VerifiableCredential",
+			"credentialSubject": {
+				"id": "did:example:ebfeb1f712ebc6f1c276e12ec21"
+			},
+			"issuer": {
+				"id": "did:example:76e12ec712ebc6f1c221ebfeb1f",
+				"name": "Example University"
+			},
+			"issuanceDate": "2010-01-01T19:23:24Z",
+			"credentialStatus": {
+				"id": "https://example.gov/status/24",
+				"type": "CredentialStatusList2017"
+			}
+		}],
+		"holder": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+		"refreshService": {
+			"id": "https://example.edu/refresh/3732",
+			"type": "ManualRefreshService2018"
+		}
+	}`
 )
 
 func TestNew(t *testing.T) {
 	svc := New(&Config{})
 	require.NotNil(t, svc)
 	require.Equal(t, 2, len(svc.GetRESTHandlers()))
-}
-
-func TestHTTPPost(t *testing.T) {
-	t.Run("test error from http post", func(t *testing.T) {
-		svc := New(&Config{})
-		svc.client = &mockHTTPClient{postErr: fmt.Errorf("post error")}
-		data, err := svc.httpPost("", "", []byte(""))
-		require.Error(t, err)
-		require.Contains(t, err.Error(), "post error")
-		require.Empty(t, data)
-	})
-
-	t.Run("test response status not ok", func(t *testing.T) {
-		svc := New(&Config{})
-		svc.client = &mockHTTPClient{postValue: &http.Response{StatusCode: http.StatusInternalServerError}}
-		data, err := svc.httpPost("", "", []byte(""))
-		require.Error(t, err)
-		require.Contains(t, err.Error(), "received unsuccessful POST HTTP status")
-		require.Empty(t, data)
-	})
-
-	t.Run("test success", func(t *testing.T) {
-		svc := New(&Config{})
-		svc.client = &mockHTTPClient{postValue: &http.Response{
-			StatusCode: http.StatusOK, Body: ioutil.NopCloser(strings.NewReader("data"))}}
-		data, err := svc.httpPost("", "", []byte(""))
-		require.NoError(t, err)
-		require.Equal(t, "data", data)
-	})
 }
 
 func TestVerifyVC(t *testing.T) {
@@ -66,56 +94,54 @@ func TestVerifyVC(t *testing.T) {
 		require.Contains(t, rr.Body.String(), "failed to parse form")
 	})
 
-	t.Run("test error from http post", func(t *testing.T) {
+	t.Run("test error from unmarshal request", func(t *testing.T) {
 		svc := New(&Config{})
-		svc.client = &mockHTTPClient{postErr: fmt.Errorf("post error")}
-
-		rr := httptest.NewRecorder()
-		m := make(map[string][]string)
-		m["vcDataInput"] = []string{"vc"}
-		svc.verifyVC(rr, &http.Request{Form: m})
-		require.Equal(t, http.StatusBadRequest, rr.Code)
-		require.Contains(t, rr.Body.String(), "post error")
-	})
-
-	t.Run("test error from unmarshal post", func(t *testing.T) {
-		svc := New(&Config{})
-		svc.client = &mockHTTPClient{postValue: &http.Response{
-			StatusCode: http.StatusOK, Body: ioutil.NopCloser(strings.NewReader("data"))}}
 
 		rr := httptest.NewRecorder()
 		m := make(map[string][]string)
 		m["vcDataInput"] = []string{"vc"}
 		svc.verifyVC(rr, &http.Request{Form: m})
 		require.Equal(t, http.StatusInternalServerError, rr.Code)
-		require.Contains(t, rr.Body.String(), "failed to unmarshal")
+		require.Contains(t, rr.Body.String(), "failed to unmarshal request")
+	})
+
+	t.Run("test error from http post", func(t *testing.T) {
+		svc := New(&Config{})
+		svc.client = &mockHTTPClient{postErr: fmt.Errorf("post error")}
+
+		rr := httptest.NewRecorder()
+		m := make(map[string][]string)
+		svc.verifyVC(rr, &http.Request{Form: m})
+		require.Equal(t, http.StatusBadRequest, rr.Code)
+		require.Contains(t, rr.Body.String(), "post error")
 	})
 
 	t.Run("test verify vc failed", func(t *testing.T) {
 		svc := New(&Config{})
-		b, err := json.Marshal(verifyResponse{Verified: false, Message: "failed to verify"})
+		b, err := json.Marshal(edgesvcops.CredentialsVerificationFailResponse{
+			Checks: []edgesvcops.CredentialsVerificationCheckResult{
+				{Check: "status", Error: "status check failed"},
+			},
+		})
 		require.NoError(t, err)
 		svc.client = &mockHTTPClient{postValue: &http.Response{
-			StatusCode: http.StatusOK, Body: ioutil.NopCloser(strings.NewReader(string(b)))}}
+			StatusCode: http.StatusBadRequest, Body: ioutil.NopCloser(strings.NewReader(string(b)))}}
 
 		rr := httptest.NewRecorder()
 		m := make(map[string][]string)
-		m["vcDataInput"] = []string{"vc"}
 		svc.verifyVC(rr, &http.Request{Form: m})
 		require.Equal(t, http.StatusBadRequest, rr.Code)
-		require.Contains(t, rr.Body.String(), "failed to verify")
+		require.Contains(t, rr.Body.String(), "status check failed")
 	})
 
 	t.Run("test vc html not exist", func(t *testing.T) {
 		svc := New(&Config{VCHTML: ""})
-		b, err := json.Marshal(verifyResponse{Verified: true})
-		require.NoError(t, err)
 		svc.client = &mockHTTPClient{postValue: &http.Response{
-			StatusCode: http.StatusOK, Body: ioutil.NopCloser(strings.NewReader(string(b)))}}
+			StatusCode: http.StatusOK, Body: nil}}
 
 		rr := httptest.NewRecorder()
 		m := make(map[string][]string)
-		m["vcDataInput"] = []string{"vc"}
+		m["vcDataInput"] = []string{validVC}
 		svc.verifyVC(rr, &http.Request{Form: m})
 		require.Equal(t, http.StatusInternalServerError, rr.Code)
 		require.Contains(t, rr.Body.String(), "unable to load html")
@@ -127,14 +153,12 @@ func TestVerifyVC(t *testing.T) {
 
 		defer func() { require.NoError(t, os.Remove(file.Name())) }()
 		svc := New(&Config{VCHTML: file.Name()})
-		b, err := json.Marshal(verifyResponse{Verified: true})
-		require.NoError(t, err)
 		svc.client = &mockHTTPClient{postValue: &http.Response{
-			StatusCode: http.StatusOK, Body: ioutil.NopCloser(strings.NewReader(string(b)))}}
+			StatusCode: http.StatusOK, Body: nil}}
 
 		rr := httptest.NewRecorder()
 		m := make(map[string][]string)
-		m["vcDataInput"] = []string{"vc"}
+		m["vcDataInput"] = []string{validVC}
 
 		svc.verifyVC(rr, &http.Request{Form: m})
 		require.Equal(t, http.StatusOK, rr.Code)
@@ -152,18 +176,6 @@ func TestVerifyVP(t *testing.T) {
 		require.Contains(t, rr.Body.String(), "failed to parse form: missing form body")
 	})
 
-	t.Run("test error due to invalid form data", func(t *testing.T) {
-		svc := New(&Config{})
-		svc.client = &mockHTTPClient{postErr: fmt.Errorf("invalid form data")}
-
-		rr := httptest.NewRecorder()
-		m := make(map[string][]string)
-		m["vpDataInput"] = []string{"vp"}
-		svc.verifyVP(rr, &http.Request{Form: m})
-		require.Equal(t, http.StatusBadRequest, rr.Code)
-		require.Contains(t, rr.Body.String(), "failed to verify: invalid form data")
-	})
-
 	t.Run("test error from unmarshal post", func(t *testing.T) {
 		svc := New(&Config{})
 		svc.client = &mockHTTPClient{postValue: &http.Response{
@@ -174,37 +186,66 @@ func TestVerifyVP(t *testing.T) {
 		m["vpDataInput"] = []string{"vp"}
 		svc.verifyVP(rr, &http.Request{Form: m})
 		require.Equal(t, http.StatusInternalServerError, rr.Code)
-		require.Contains(t, rr.Body.String(), "failed to unmarshal")
+		require.Contains(t, rr.Body.String(), "failed to unmarshal request")
+	})
+
+	t.Run("test error due to invalid form data", func(t *testing.T) {
+		svc := New(&Config{})
+		svc.client = &mockHTTPClient{postErr: fmt.Errorf("invalid form data")}
+
+		rr := httptest.NewRecorder()
+		m := make(map[string][]string)
+		svc.verifyVP(rr, &http.Request{Form: m})
+		require.Equal(t, http.StatusBadRequest, rr.Code)
+		require.Contains(t, rr.Body.String(), "failed to verify: invalid form data")
 	})
 
 	t.Run("test verify vp failed", func(t *testing.T) {
 		svc := New(&Config{})
-		b, err := json.Marshal(verifyResponse{Verified: false, Message: "failed to verify"})
+		b, err := json.Marshal(edgesvcops.CredentialsVerificationFailResponse{
+			Checks: []edgesvcops.CredentialsVerificationCheckResult{
+				{Check: "status", Error: "status check failed"},
+			},
+		})
 		require.NoError(t, err)
 		svc.client = &mockHTTPClient{postValue: &http.Response{
-			StatusCode: http.StatusOK, Body: ioutil.NopCloser(strings.NewReader(string(b)))}}
+			StatusCode: http.StatusBadRequest, Body: ioutil.NopCloser(strings.NewReader(string(b)))}}
 
 		rr := httptest.NewRecorder()
 		m := make(map[string][]string)
-		m["vpDataInput"] = []string{"vp"}
 		svc.verifyVP(rr, &http.Request{Form: m})
 		require.Equal(t, http.StatusBadRequest, rr.Code)
-		require.Contains(t, rr.Body.String(), "failed to verify")
+		require.Contains(t, rr.Body.String(), "status check failed")
 	})
 
 	t.Run("test vp html not exist", func(t *testing.T) {
 		svc := New(&Config{VCHTML: ""})
-		b, err := json.Marshal(verifyResponse{Verified: true})
-		require.NoError(t, err)
 		svc.client = &mockHTTPClient{postValue: &http.Response{
-			StatusCode: http.StatusOK, Body: ioutil.NopCloser(strings.NewReader(string(b)))}}
+			StatusCode: http.StatusOK, Body: nil}}
 
 		rr := httptest.NewRecorder()
 		m := make(map[string][]string)
-		m["vpDataInput"] = []string{"vp"}
+		m["vcDataInput"] = []string{validVP}
 		svc.verifyVP(rr, &http.Request{Form: m})
 		require.Equal(t, http.StatusInternalServerError, rr.Code)
 		require.Contains(t, rr.Body.String(), "unable to load html")
+	})
+
+	t.Run("test resp read fail", func(t *testing.T) {
+		file, err := ioutil.TempFile("", "*.html")
+		require.NoError(t, err)
+
+		defer func() { require.NoError(t, os.Remove(file.Name())) }()
+		svc := New(&Config{VPHTML: file.Name()})
+		svc.client = &mockHTTPClient{postValue: &http.Response{
+			StatusCode: http.StatusOK, Body: nil}}
+
+		rr := httptest.NewRecorder()
+		m := make(map[string][]string)
+		m["vcDataInput"] = []string{validVP}
+
+		svc.verifyVP(rr, &http.Request{Form: m})
+		require.Equal(t, http.StatusOK, rr.Code)
 	})
 
 	t.Run("test success", func(t *testing.T) {
@@ -213,14 +254,12 @@ func TestVerifyVP(t *testing.T) {
 
 		defer func() { require.NoError(t, os.Remove(file.Name())) }()
 		svc := New(&Config{VPHTML: file.Name()})
-		b, err := json.Marshal(verifyResponse{Verified: true})
-		require.NoError(t, err)
 		svc.client = &mockHTTPClient{postValue: &http.Response{
-			StatusCode: http.StatusOK, Body: ioutil.NopCloser(strings.NewReader(string(b)))}}
+			StatusCode: http.StatusOK, Body: nil}}
 
 		rr := httptest.NewRecorder()
 		m := make(map[string][]string)
-		m["vpDataInput"] = []string{"vp"}
+		m["vcDataInput"] = []string{validVP}
 
 		svc.verifyVP(rr, &http.Request{Form: m})
 		require.Equal(t, http.StatusOK, rr.Code)
