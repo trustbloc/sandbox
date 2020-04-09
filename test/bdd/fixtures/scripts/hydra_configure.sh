1798da38511a725ee4b80425f227e6d4ab13552c
talwinder.kaur <talwinder.kaur@securekey.com>
2020-04-09 17:36:01 -0400

    feat: Add PRC Data flow
    closes #212
    
    Signed-off-by: talwinder.kaur <talwinder.kaur@securekey.com>

--- test/bdd/fixtures/scripts/hydra_configure.sh
+++ test/bdd/fixtures/scripts/hydra_configure.sh
@@ -14,7 +14,7 @@ hydra clients create \
     --secret secret \
     --grant-types authorization_code,refresh_token \
     --response-types code,id_token \
-    --scope StudentCard,TravelCard \
+    --scope StudentCard,TravelCard,PRCard \
     --skip-tls-verify \
     --callbacks https://issuer.trustbloc.local/callback
 echo "Finish Creating client"
