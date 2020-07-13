ad4b81fa4c4b6b91612f9aa667fe8528368f93f8
Rolson Quadras <rolson.quadras@securekey.com>
2020-07-13 09:34:49 -0400

    chore: Update Issuer adapter
    Signed-off-by: Rolson Quadras <rolson.quadras@securekey.com>

--- test/bdd/fixtures/scripts/didcomm/issuer_adapter_configure.sh
+++ test/bdd/fixtures/scripts/didcomm/issuer_adapter_configure.sh
@@ -15,7 +15,7 @@ until [ $n -ge $maxAttempts ]
 do
    responseCreatedTime=$(curl --header "Content-Type: application/json" \
    --request POST \
-   --data '{"id":"tb-cc-issuer", "name":"TrustBloc - Credit Card Data Issuer", "callbackURL":"https://issuer.trustbloc.local/didcomm/cb"}' \
+   --data '{"id":"tb-cc-issuer", "name":"TrustBloc - Credit Card Data Issuer", "url":"https://issuer.trustbloc.local/didcomm/", "supportedVCContexts" : ["https://trustbloc.github.io/context/vc/examples-ext-v1.jsonld"]}' \
    http://issuer.adapter.rest.example.com:10061/profile | jq -r '.createdAt' 2>/dev/null)
    echo "'created' field from profile tb-cc-issuer response is: $responseCreatedTime"
 
