9f97e26cf1493b6b4db76c5f9216b66630d01ffe
Rolson Quadras <rolson.quadras@securekey.com>
2020-08-11 13:43:08 -0400

    fix: DL data setup
    Signed-off-by: Rolson Quadras <rolson.quadras@securekey.com>

--- test/bdd/fixtures/scripts/strapi_configure.sh
+++ test/bdd/fixtures/scripts/strapi_configure.sh
@@ -184,7 +184,7 @@ fi
 # Add driving license data for above created user. Driving license data settup todo issue-449
 result=$(curl --header "Content-Type: application/json" --header "Authorization: Bearer $token" \
    --request POST \
-   --data '{"userid":"100","vcmetadata":{"@context":["https://www.w3.org/2018/credentials/v1","https://trustbloc.github.io/context/vc/examples/driving-licence-v1.jsonld"],"name":"Drivers Licence","description":"Drivers Licence for Mr.Foo"},"vccredentialsubject":{"id":"1234568","name":"Foo","licenceType":"G2","issuedDate":"2020-05-27","expiryDate":"2025-05-26","address":"4726 Pine Street, Toronto - A1B 2C3","type":"DrivingLicence"}}' \
+   --data '{"userid":"100","vcmetadata":{"@context":["https://www.w3.org/2018/credentials/v1","https://trustbloc.github.io/context/vc/examples/driving-license-v1.jsonld"],"name":"Drivers Licence","description":"Drivers Licence for Mr.Foo"},"vccredentialsubject":{"id":"1234568","name":"Foo","licenceType":"G2","issuedDate":"2020-05-27","expiryDate":"2025-05-26","address":"4726 Pine Street, Toronto - A1B 2C3"}}' \
    http://strapi:1337/drivinglicenses | jq  -r ".error")
 # check for error
 if [ "$result" != "null" ]
