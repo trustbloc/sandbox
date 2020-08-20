485b1d5afba91b701fa2e3f5fa9a497ae3b772b6
George Aristy <george.aristy@securekey.com>
2020-08-20 17:52:04 -0400

    feat: #494 update rp-adapter && edge-agent
    * edge-agent:
      * can now select issuer manifest credential from a schema uri inside
      constraints: trustbloc/edge-agent#295
      * credential storage (plaintext for now) trustbloc/edge-agent#285
      * revert CHAPI mediator customizations trustbloc/edge-agent#302
      * fix invalid json paths in presentation submission
      trustbloc/edge-agent#301
    * rp-adapter:
      * handle local and remote credentials trustbloc/edge-adapter#257
      * map credentialSubject to RP object `verified_claims`
      trustbloc/edge-adapter#161
      * fix error parsing VCs in response trustbloc/edge-adapter#270
      * fix panic on startup when GOVERNANCE_VCS URL is not provided
      trustbloc/edge-adapter#266
      * fix presentation-exchange schema uri should be an array
      trustbloc/edge-adapter#256
    
    Signed-off-by: George Aristy <george.aristy@securekey.com>

--- test/bdd/fixtures/scripts/rp-rest_start.sh
+++ test/bdd/fixtures/scripts/rp-rest_start.sh
@@ -19,7 +19,7 @@ registerRPTenant() {
         response=$(curl -k -o - -s -w "RESPONSE_CODE=%{response_code}" \
         --header "Content-Type: application/json" \
         --request POST \
-        --data '{"label": "rp.trustbloc.local", "callback": "'$callbackURL'", "scopes": ["CreditCardStatement","DrivingLicense"]}' \
+        --data '{"label": "rp.trustbloc.local", "callback": "'$callbackURL'", "scopes": ["credit_card_stmt:remote","driver_license:local"]}' \
         $rpAdapterURL)
 
         code=${response//*RESPONSE_CODE=/}
