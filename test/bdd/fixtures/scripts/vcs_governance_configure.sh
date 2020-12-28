bf662655f8841103efb1b837b293349603666611
Firas Qutishat <firas.qutishat@securekey.com>
2020-12-28 09:56:21 -0500

    chore: Improve local sandbox to run better on developer machines
    closes #708
    
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- test/bdd/fixtures/scripts/vcs_governance_configure.sh
+++ test/bdd/fixtures/scripts/vcs_governance_configure.sh
@@ -1,11 +1,28 @@
-#!/bin/sh
 #
 # Copyright SecureKey Technologies Inc. All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-apk --no-cache add curl jq
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
 
-# governance profiles
-/tmp/scripts/vcs/governance-trustbloc-profiles.sh
+trustbloc_governance=$(curl -o /dev/null -s -w "%{http_code}" --header "Content-Type: application/json" --header "Authorization: Bearer vcs_governance_rw_token" \
+   --request POST \
+   --data '{"name":"governance", "uri":"http://example.com", "signatureType":"Ed25519Signature2018", "signatureRepresentation":1,"didKeyType":"Ed25519"}' \
+   https://governance-vcs.trustbloc.local/governance/profile)
+
+checkProfileIsCreated()
+{
+   if [ "$1" == "201" ]
+   then
+     echo "${GREEN} governance profile $2 is created"
+     tput init
+   else
+     echo "${RED}failed create governance profile $2 response code $1"
+     tput init
+     exit -1
+   fi
+}
+
+checkProfileIsCreated $trustbloc_governance trustbloc-governance
