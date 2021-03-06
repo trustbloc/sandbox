7ef508c24fdf50cb112fb750f7e462742a474cdf
Filip Burlacu <filip.burlacu@securekey.com>
2021-03-05 19:43:39 -0500

    feat: issuer adapter does an OIDC login for an access & refresh token.
    Signed-off-by: Filip Burlacu <filip.burlacu@securekey.com>

--- test/bdd/fixtures/scripts/didcomm/issuer_adapter_configure.sh
+++ test/bdd/fixtures/scripts/didcomm/issuer_adapter_configure.sh
@@ -15,7 +15,7 @@ until [ $n -ge $maxAttempts ]
 do
    responseCreatedTime=$(curl -k --header "Content-Type: application/json" \
    --request POST \
-   --data '{"id":"tb-cc-issuer", "name":"TrustBloc - Credit Card Data Issuer", "url":"https://issuer.trustbloc.local/didcomm", "supportedVCContexts" : ["https://trustbloc.github.io/context/vc/examples/credit-card-v1.jsonld"]}' \
+   --data '{"id":"tb-cc-issuer", "name":"TrustBloc - Credit Card Data Issuer", "url":"https://issuer.trustbloc.local/didcomm", "oidcProvider":"https://hydra.trustbloc.local/",  "supportedVCContexts" : ["https://trustbloc.github.io/context/vc/examples/credit-card-v1.jsonld"]}' \
    --insecure https://issuer.adapter.rest.example.com:10061/profile | jq -r '.createdAt' 2>/dev/null)
    echo "'created' field from profile tb-cc-issuer response is: $responseCreatedTime"
 
@@ -40,7 +40,7 @@ until [ $n -ge $maxAttempts ]
 do
    responseCreatedTime=$(curl -k --header "Content-Type: application/json" \
    --request POST \
-   --data '{"id":"tb-cr-issuer", "name":"TrustBloc - Credit Report Issuer", "url":"https://issuer.trustbloc.local/didcomm", "supportedVCContexts" : ["https://trustbloc.github.io/context/vc/examples/credit-score-v1.jsonld"], "requiresBlindedRoute": true}' \
+   --data '{"id":"tb-cr-issuer", "name":"TrustBloc - Credit Report Issuer", "url":"https://issuer.trustbloc.local/didcomm", "oidcProvider":"https://hydra.trustbloc.local/", "supportedVCContexts" : ["https://trustbloc.github.io/context/vc/examples/credit-score-v1.jsonld"], "requiresBlindedRoute": true}' \
    --insecure https://issuer.adapter.rest.example.com:10061/profile | jq -r '.createdAt' 2>/dev/null)
    echo "'created' field from profile tb-cr-issuer response is: $responseCreatedTime"
 
@@ -64,7 +64,7 @@ until [ $n -ge $maxAttempts ]
 do
    responseCreatedTime=$(curl -k --header "Content-Type: application/json" \
    --request POST \
-   --data '{"id":"tb-dl-issuer", "name":"TrustBloc - Driving License + Assurance Issuer", "url":"https://issuer.trustbloc.local/didcomm", "supportedVCContexts" : ["https://trustbloc.github.io/context/vc/examples/driver-license-evidence-v1.jsonld"], "supportsAssuranceCredential" : true, "requiresBlindedRoute": true}' \
+   --data '{"id":"tb-dl-issuer", "name":"TrustBloc - Driving License + Assurance Issuer", "url":"https://issuer.trustbloc.local/didcomm", "oidcProvider":"https://hydra.trustbloc.local/", "supportedVCContexts" : ["https://trustbloc.github.io/context/vc/examples/driver-license-evidence-v1.jsonld"], "supportsAssuranceCredential" : true, "requiresBlindedRoute": true}' \
    --insecure https://issuer.adapter.rest.example.com:10061/profile | jq -r '.createdAt' 2>/dev/null)
    echo "'created' field from profile tb-dl-issuer response is: $responseCreatedTime"
 
