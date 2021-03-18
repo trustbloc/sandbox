23935314e5f40df0e5517a7d4cbbef03d055b22e
Firas Qutishat <firas.qutishat@securekey.com>
2021-03-18 15:14:34 -0400

    chore: create sandbox-cli
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- test/bdd/cmd/demo/main.go
+++ test/bdd/cmd/demo/main.go
@@ -7,94 +7,28 @@ package main
 
 import (
 	"crypto/ed25519"
-	"crypto/tls"
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/btcsuite/btcutil/base58"
-	httptransport "github.com/go-openapi/runtime/client"
-	"github.com/go-openapi/strfmt"
 	"github.com/square/go-jose/v3"
-	"github.com/trustbloc/edge-service/pkg/client/comparator/client"
-	"github.com/trustbloc/edge-service/pkg/client/comparator/client/operations"
 )
 
-const (
-	requestTimeout = 20 * time.Second
-)
-
-type configOutput struct {
-	DID        string `json:"did"`
-	PrivateKey string `json:"privateKey"`
-	KeyID      string `json:"keyID"`
-}
-
-func main() { //nolint: funlen
+func main() {
 	switch os.Args[1] { //nolint: gocritic
-	case "comparator":
-		switch os.Args[2] { //nolint: gocritic
-		case "getConfig":
-			//nolint: gosec
-			httpClient := &http.Client{
-				Transport: &http.Transport{
-					TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-				},
-			}
-
-			comparatorURL := strings.Split(os.Args[3], "://")
-
-			transport := httptransport.NewWithClient(
-				comparatorURL[1],
-				client.DefaultBasePath,
-				[]string{comparatorURL[0]},
-				httpClient,
-			)
-
-			c := client.New(transport, strfmt.Default)
-
-			config, err := c.Operations.GetConfig(operations.NewGetConfigParams().
-				WithTimeout(requestTimeout))
-			if err != nil {
-				fmt.Printf("failed to create new request: %s\n", err)
-				return
-			}
-
-			keys, ok := config.Payload.Key.([]interface{})
-			if !ok {
-				fmt.Printf("key is not array\n")
-				return
-			}
-
-			keyBytes, err := json.Marshal(keys[0])
-			if err != nil {
-				fmt.Printf("failed to marshal key: %s\n", err)
-				return
-			}
-
-			jwk := jose.JSONWebKey{}
-			if errUnmarshalJSON := jwk.UnmarshalJSON(keyBytes); errUnmarshalJSON != nil {
-				fmt.Printf("failed to unmarshal resp to jwk: %s\n", errUnmarshalJSON)
-				return
-			}
-
-			k, ok := jwk.Key.(ed25519.PrivateKey)
-			if !ok {
-				fmt.Printf("key is not ed25519\n")
-				return
-			}
-
-			bytes, err := json.Marshal(configOutput{DID: *config.Payload.Did, PrivateKey: base58.Encode(k),
-				KeyID: fmt.Sprintf("%s#%s", *config.Payload.Did, jwk.KeyID)})
-			if err != nil {
-				fmt.Printf("failed to marshal: %s\n", err)
-				return
-			}
-
-			fmt.Println(string(bytes))
+	case "getPrivateKey":
+		jwk := jose.JSONWebKey{}
+		if errUnmarshalJSON := jwk.UnmarshalJSON([]byte(os.Args[2])); errUnmarshalJSON != nil {
+			fmt.Printf("failed to unmarshal resp to jwk: %s\n", errUnmarshalJSON)
+			return
 		}
+
+		k, ok := jwk.Key.(ed25519.PrivateKey)
+		if !ok {
+			fmt.Printf("key is not ed25519\n")
+			return
+		}
+
+		fmt.Println(base58.Encode(k))
 	}
 }
--- test/bdd/fixtures/scripts/ace_rp_configure.sh
+++ test/bdd/fixtures/scripts/ace_rp_configure.sh
@@ -22,15 +22,16 @@ validateProfileCreation()
 }
 
 # ucis - configure comparator
-ucisComparatorConfig=$(.build/bin/demo comparator getConfig https://ucis-comparator.trustbloc.local)
+ucisComparatorConfig=$(curl -s --header "Content-Type: application/json" --request GET --insecure https://ucis-comparator.trustbloc.local/config)
 ucisComparatorConfigDID=$(echo "${ucisComparatorConfig}" | jq -r '.did')
-ucisComparatorConfigPrivateKey=$(echo "${ucisComparatorConfig}" | jq -r '.privateKey')
-ucisComparatorConfigKeyID=$(echo "${ucisComparatorConfig}" | jq -r '.keyID')
+ucisComparatorConfigKeyID=$(echo "${ucisComparatorConfig}" | jq -r '.key[0].kid')
+jwk=$(echo "${ucisComparatorConfig}" | jq -r '.key[0]')
+ucisComparatorConfigPrivateKey=$(docker run --rm  ghcr.io/trustbloc/sandbox-cli:latest getPrivateKey "$jwk")
 
 # ucis - create vc issuer profile
 vc_issuer_ucis=$(curl -k -o - -s -w "RESPONSE_CODE=%{response_code}" --header "Content-Type: application/json" --header "Authorization: Bearer vcs_issuer_rw_token" \
    --request POST \
-   --data '{"name":"vc-issuer-ucis", "uri":"http://example.com", "signatureType":"Ed25519Signature2018", "did":"'"${ucisComparatorConfigDID}"'","didPrivateKey":"'"${ucisComparatorConfigPrivateKey}"'","didKeyID":"'"${ucisComparatorConfigKeyID}"'","signatureRepresentation":1,"didKeyType":"Ed25519"}' \
+   --data '{"name":"vc-issuer-ucis", "uri":"http://example.com", "signatureType":"Ed25519Signature2018", "did":"'"${ucisComparatorConfigDID}"'","didPrivateKey":"'"${ucisComparatorConfigPrivateKey}"'","didKeyID":"'"${ucisComparatorConfigDID}"'#'"${ucisComparatorConfigKeyID}"'","signatureRepresentation":1,"didKeyType":"Ed25519"}' \
    --insecure https://issuer-vcs.trustbloc.local/profile)
 
 response=${vc_issuer_ucis//RESPONSE_CODE*/}
@@ -39,15 +40,16 @@ code=${vc_issuer_ucis//*RESPONSE_CODE=/}
 validateProfileCreation $code $response vc_issuer vc_issuer_ucis
 
 # cbp - configure comparator
-cbpComparatorConfig=$(.build/bin/demo comparator getConfig https://cbp-comparator.trustbloc.local)
+cbpComparatorConfig=$(curl -s --header "Content-Type: application/json" --request GET --insecure https://cbp-comparator.trustbloc.local/config)
 cbpComparatorConfigDID=$(echo "${cbpComparatorConfig}" | jq -r '.did')
-cbpComparatorConfigPrivateKey=$(echo "${cbpComparatorConfig}" | jq -r '.privateKey')
-cbpComparatorConfigKeyID=$(echo "${cbpComparatorConfig}" | jq -r '.keyID')
+cbpComparatorConfigKeyID=$(echo "${cbpComparatorConfig}" | jq -r '.key[0].kid')
+cbpComparatorJWK=$(echo "${cbpComparatorConfig}" | jq -r '.key[0]')
+cbpComparatorConfigPrivateKey=$(docker run --rm  ghcr.io/trustbloc/sandbox-cli:latest getPrivateKey "$cbpComparatorJWK")
 
 # cbp - create vc issuer profile
 vc_issuer_cbp=$(curl -k -o - -s -w "RESPONSE_CODE=%{response_code}" --header "Content-Type: application/json" --header "Authorization: Bearer vcs_issuer_rw_token" \
    --request POST \
-   --data '{"name":"vc-issuer-cbp", "uri":"http://example.com", "signatureType":"Ed25519Signature2018", "did":"'"${cbpComparatorConfigDID}"'","didPrivateKey":"'"${cbpComparatorConfigPrivateKey}"'","didKeyID":"'"${cbpComparatorConfigKeyID}"'","signatureRepresentation":1,"didKeyType":"Ed25519"}' \
+   --data '{"name":"vc-issuer-cbp", "uri":"http://example.com", "signatureType":"Ed25519Signature2018", "did":"'"${cbpComparatorConfigDID}"'","didPrivateKey":"'"${cbpComparatorConfigPrivateKey}"'","didKeyID":"'"${cbpComparatorConfigDID}"'#'"${cbpComparatorConfigKeyID}"'","signatureRepresentation":1,"didKeyType":"Ed25519"}' \
    --insecure https://issuer-vcs.trustbloc.local/profile)
 
 response=${vc_issuer_cbp//RESPONSE_CODE*/}
@@ -56,15 +58,16 @@ code=${vc_issuer_cbp//*RESPONSE_CODE=/}
 validateProfileCreation $code $response vc_issuer vc_issuer_cbp
 
 # benefits-dept - configure comparator
-benefitsDeptComparatorConfig=$(.build/bin/demo comparator getConfig https://benefits-dept-comparator.trustbloc.local)
+benefitsDeptComparatorConfig=$(curl -s --header "Content-Type: application/json" --request GET --insecure https://benefits-dept-comparator.trustbloc.local/config)
 benefitsDeptComparatorConfigDID=$(echo "${benefitsDeptComparatorConfig}" | jq -r '.did')
-benefitsDeptComparatorConfigPrivateKey=$(echo "${benefitsDeptComparatorConfig}" | jq -r '.privateKey')
-benefitsDeptComparatorConfigKeyID=$(echo "${benefitsDeptComparatorConfig}" | jq -r '.keyID')
+benefitsDeptComparatorConfigKeyID=$(echo "${benefitsDeptComparatorConfig}" | jq -r '.key[0].kid')
+benefitsDeptComparatorJWK=$(echo "${benefitsDeptComparatorConfig}" | jq -r '.key[0]')
+benefitsDeptComparatorConfigPrivateKey=$(docker run --rm  ghcr.io/trustbloc/sandbox-cli:latest getPrivateKey "$benefitsDeptComparatorJWK")
 
 # benefits-dept - create vc issuer profile
 vc_issuer_benefits_dept=$(curl -k -o - -s -w "RESPONSE_CODE=%{response_code}" --header "Content-Type: application/json" --header "Authorization: Bearer vcs_issuer_rw_token" \
    --request POST \
-   --data '{"name":"vc-issuer-benefits-dept", "uri":"http://example.com", "signatureType":"Ed25519Signature2018", "did":"'"${benefitsDeptComparatorConfigDID}"'","didPrivateKey":"'"${benefitsDeptComparatorConfigPrivateKey}"'","didKeyID":"'"${benefitsDeptComparatorConfigKeyID}"'","signatureRepresentation":1,"didKeyType":"Ed25519"}' \
+   --data '{"name":"vc-issuer-benefits-dept", "uri":"http://example.com", "signatureType":"Ed25519Signature2018", "did":"'"${benefitsDeptComparatorConfigDID}"'","didPrivateKey":"'"${benefitsDeptComparatorConfigPrivateKey}"'","didKeyID":"'"${benefitsDeptComparatorConfigDID}"'#'"${benefitsDeptComparatorConfigKeyID}"'","signatureRepresentation":1,"didKeyType":"Ed25519"}' \
    --insecure https://issuer-vcs.trustbloc.local/profile)
 
 response=${vc_issuer_benefits_dept//RESPONSE_CODE*/}
