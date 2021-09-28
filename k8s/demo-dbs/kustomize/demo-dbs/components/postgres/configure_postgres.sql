4c70b79830d40ad17a30daa6bf95cc4684095da0
Derek Trider <derek.trider@securekey.com>
2021-09-27 21:51:00 -0400

    feat: Switch Hydra from MySQL to PostgreSQL
    Also added a PostgreSQL database instance for demo components, similar to MongoDB and MySQL.
    
    Signed-off-by: Derek Trider <Derek.Trider@securekey.com>

---
+++ k8s/demo-dbs/kustomize/demo-dbs/components/postgres/configure_postgres.sql
@@ -0,0 +1,8 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+CREATE USER loginconsent with encrypted password 'loginconsent-secret-pw';
+CREATE DATABASE loginconsent;
