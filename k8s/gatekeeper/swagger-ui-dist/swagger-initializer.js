9651dcc32849b832b9958ebdad214e5a77d2e16c
Yevgen Pukhta <eugene.pukhta@gmail.com>
2022-08-22 10:16:00 +0300

    Move the swagger file to the gatekeeper folder - 2

--- k8s/gatekeeper/swagger-ui-dist/swagger-initializer.js
+++ k8s/gatekeeper/swagger-ui-dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "../gatekeeper-openapi-spec.yml",
+    url: "gatekeeper-openapi-spec.yml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
