307cdbf2bce9f2363644dcaabefc162aa4758d6a
Rolson Quadras <rolson.quadras@avast.com>
2022-09-13 09:40:03 -0400

    chore: k8s update (vcs)
    Signed-off-by: Rolson Quadras <rolson.quadras@avast.com>

--- k8s/scripts/core_deployment.sh
+++ k8s/scripts/core_deployment.sh
@@ -14,8 +14,8 @@ rm -rf $core_dir
 mkdir -p $core_dir
 cd $core_dir
 
-git clone -b main https://github.com/trustbloc/k8s $core_dir
-git checkout ${TRUSTBLOC_CORE_K8S_COMMIT}
+git clone -b vcs https://github.com/rolsonquadras/k8s $core_dir
+git checkout e65aecb4f7e3797a682d2e1f8622f5bf3985f137
 
 # uncomment below line to link deployments to https://github.com/trustbloc/k8s directly (assuming k8s repo is in same folder as sandbox)
 # rm -rf $core_dir && mkdir -p $core_dir && ln -s ../../../k8s/* $core_dir
