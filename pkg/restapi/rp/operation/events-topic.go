5ca43ebe67e8444e5839c3e01574a7b58b5d88ee
Firas Qutishat <firas.qutishat@securekey.com>
2022-10-24 23:14:50 +0300

    fix: pulling event by tx id (#1577)
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>
    
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- pkg/restapi/rp/operation/events-topic.go
+++ pkg/restapi/rp/operation/events-topic.go
@@ -1,43 +1,66 @@
 package operation
 
 import (
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"time"
 )
 
 const (
-	topicsSize   = 5000
+	topicsSize   = 10
 	topicTimeout = 1000 * time.Millisecond
 )
 
+type event struct {
+	// Data defines message(required).
+	Data eventData `json:"data"`
+}
+
+type eventData struct {
+	TXID string `json:"txID"`
+}
+
 // EventsTopic event topic.
 type EventsTopic struct {
-	topics chan []byte
+	topics map[string]chan []byte
 }
 
 // NewEventsTopic return new event topic.
 func NewEventsTopic() *EventsTopic {
 	return &EventsTopic{
-		topics: make(chan []byte, topicsSize),
+		topics: make(map[string]chan []byte),
 	}
 }
 
 func (e *EventsTopic) receiveTopics(w http.ResponseWriter, r *http.Request) {
-	msg, err := ioutil.ReadAll(r.Body)
+	msg, err := io.ReadAll(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 	}
 
 	logger.Infof("received topic message: %s", string(msg))
 
-	e.topics <- msg
+	d := &event{}
+	if err := json.Unmarshal(msg, d); err != nil {
+		fmt.Fprintf(w, `{"error":"failed unmarshal event, cause: %s"}`, err)
+	}
+
+	if e.topics[d.Data.TXID] == nil {
+		e.topics[d.Data.TXID] = make(chan []byte, topicsSize)
+	}
+
+	e.topics[d.Data.TXID] <- msg
 }
 
 func (e *EventsTopic) checkTopics(w http.ResponseWriter, r *http.Request) {
+	if e.topics[r.URL.Query().Get("tx")] == nil {
+		return
+	}
+
 	select {
-	case topic := <-e.topics:
+	case topic := <-e.topics[r.URL.Query().Get("tx")]:
 		_, err := w.Write(topic)
 		if err != nil {
 			fmt.Fprintf(w, `{"error":"failed to pull topics, cause: %s"}`, err)
